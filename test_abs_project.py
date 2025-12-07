0fde9fe5831a2dee548e0d09a13ca5251fc4dfb3
LaChebupel <vanyamironoff@gmail.com>
2025-12-07 18:23:17 +0300

    12.07.2025

--- test_abs_project.py
+++ test_abs_project.py
@@ -1,6 +1,12 @@
-def test_abs1():
-    assert abs(-42) == 42, "Should be absolute of number"
+import unittest
+
+class TestAbs(unittest.TestCase):
+    def test_abs1(self):
+        self.assertEqual(abs(-32), 32, "Should be equal")
+
+    def test_abs2(self):
+        self.assertEqual(abs(-34), 32, "Should be equal")
 
 if __name__ == "__main__":
-    test_abs1()
-    print("All pass")
+    unittest.main()
+        
